6cf9b7f981d8e490068bed78f88c100794f7afeb
NotoriousPark <notoriouspark111@gmail.com>
2015-03-29 15:14:15 -0400

    Added Proxy, Reference, Util, and Config

--- src/main/java/com/NotoriousPark/runes/Runes.java
+++ src/main/java/com/NotoriousPark/runes/Runes.java
@@ -1,7 +1,36 @@
 package com.NotoriousPark.runes;
 
-import cpw.mods.fml.common.Mod;
+import com.NotoriousPark.runes.configuration.ConfigurationHandler;
+import com.NotoriousPark.runes.proxy.IProxy;
+import com.NotoriousPark.runes.reference.Reference;
 
-@Mod(modid = "Runes", name = "Runes", version = "1.7.2-1.0")
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class Runes {
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static Runes instance;
+
+    @SidedProxy(clientSide = "com.NotoriousPark.runes.proxy.ClientProxy", serverSide = "com.NotoriousPark.runes.proxy.ServerProxy")
+    public static IProxy proxy;
+
+    @Mod.EventHandler
+    public void PreInitialization(FMLPreInitializationEvent event) {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+    }
+
+    @Mod.EventHandler
+    public void Initialization(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void PostInitialization(FMLPostInitializationEvent event) {
+
+    }
 }
